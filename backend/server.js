6a2d982f2c84266fabf74fba5ca435491af4c9c5
pramodlohra <pramod.lohra@nathcorp.com>
2024-09-12 16:47:39 +0530

    initial commit by Pmd

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const express = require('express');
 const dotenv = require('dotenv').config();
 const port = process.env.PORT;
@@ -8,3 +9,6 @@ app.listen(port,()=>console.log(`server is started at port${port}`));
 
 
 app.use('/api/goals',require('./routes/goalRoutes'));
+=======
+console.log("Hello world!");
+>>>>>>> 20be27e (initial commit by Pmd)
