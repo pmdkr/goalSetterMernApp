ba4d9f2f6be93307df25b0d5d514ba6e4cb77925
pramodlohra <pramod.lohra@nathcorp.com>
2024-09-13 18:31:06 +0530

    Goal controller and routes setup

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,42 +1,49 @@
+const asyncHandler = require("express-async-handler")
 // @desc Get goals
 // @routes GET /api/goals
 // @access Private
 
-const getGoals = (req, res) => {
+const getGoals = asyncHandler(async (req, res) => {
+
 
     res.status(200).json({ "message": "get my goals" });
 
-};
+});
 
 // @desc  Set new goals
 // @routes POST /api/goals
 // @access Private
 
-const setGoals = (req, res) => {
+const setGoals = asyncHandler(async (req, res) => {
+    if (!req.body.text) {
+        res.status(400);
+        throw new Error("Please add text field")
+
+    }
 
     res.status(200).json({ "message": "goal set" });
 
-};
+});
 
 // @desc Update goal
 // @routes PUT /api/goals:id
 // @access Private
 
-const updateGoals = (req, res) => {
+const updateGoals = asyncHandler(async (req, res) => {
 
     res.status(200).json({ "message": `update the goal ${req.params.id}` });
 
-};
+});
 
 // @desc detete goals
 // @routes DELETE /api/goals
 // @access Private
 
-const deleteGoals = (req, res) => {
+const deleteGoals = asyncHandler(async (req, res) => {
 
     res.status(200).json({ "message": `delted goal ${req.params.id}` });
 
-};
+});
 
 module.exports = {
     getGoals, setGoals, updateGoals, deleteGoals
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware');
 const port = process.env.PORT;
 
 
-const app=express();
-app.listen(port,()=>console.log(`server is started at port${port}`));
+const app = express();
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: false }))
+
+app.use('/api/goals', require('./routes/goalRoutes'));
+
+app.use(errorHandler)
+
+app.listen(port, () => console.log(`server is started at port ${port}`));
 
 
-app.use('/api/goals',require('./routes/goalRoutes'));
+
